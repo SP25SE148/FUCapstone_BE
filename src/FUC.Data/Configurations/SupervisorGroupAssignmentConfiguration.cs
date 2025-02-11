ab3ddf1fd8990f9b63b4fa28c984449b6b9db6e3
CGBin0305 <huyndhse173531@fpt.edu.vn>
2025-02-11 15:34:29 +0700

    add SupervisorGroupAssignment table

---
+++ src/FUC.Data/Configurations/SupervisorGroupAssignmentConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using FUC.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FUC.Data.Configurations;
+
+public sealed class SupervisorGroupAssignmentConfiguration : IEntityTypeConfiguration<SupervisorGroupAssignment>
+{
+    public void Configure(EntityTypeBuilder<SupervisorGroupAssignment> builder)
+    {
+        builder.ToTable("SupervisorGroupAssignment");
+
+        builder.HasKey(s => s.Id);
+
+        builder.Property(sga => sga.Id)
+            .IsRequired();
+
+        builder.Property(sga => sga.SupervisorId)
+            .IsRequired();
+
+        // Thiết lập quan hệ với bảng Group
+        builder.HasOne(sga => sga.Group)
+            .WithMany()
+            .HasForeignKey(sga => sga.GroupId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Thiết lập quan hệ với Supervisor (SupervisorId bắt buộc)
+        builder.HasOne(sga => sga.Supervisor)
+            .WithMany()
+            .HasForeignKey(sga => sga.SupervisorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Thiết lập quan hệ với Supervisor2 (Supervisor2Id có thể null)
+        builder.HasOne(sga => sga.Supervisor2)
+            .WithMany()
+            .HasForeignKey(sga => sga.Supervisor2Id)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ src/FUC.Data/Entities/SupervisorGroupAssignment.cs
@@ -0,0 +1,15 @@
+﻿using FUC.Data.Abstractions;
+
+namespace FUC.Data.Entities;
+
+public sealed class SupervisorGroupAssignment : AuditableSoftDeleteEntity
+{
+    public Guid Id { get; set; }
+    public Guid GroupId { get; set; }
+    public string SupervisorId { get; set; }
+    public string? Supervisor2Id { get; set; }
+
+    public Group Group { get; set; } = null!;
+    public Supervisor Supervisor { get; set; } = null!;
+    public Supervisor? Supervisor2 { get; set; }
+}
