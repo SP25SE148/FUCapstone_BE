261e60c2ce227cd6b8c887e0cfb3a5d6beccbb28
CGBin0305 <huyndhse173531@fpt.edu.vn>
2025-02-14 12:38:34 +0700

    fix academic management api response

--- src/FUC.API/Controllers/AcademicManagementController.cs
+++ src/FUC.API/Controllers/AcademicManagementController.cs
@@ -26,7 +26,7 @@ public sealed class AcademicManagementController(
     public async Task<IActionResult> GetAllCampusAsync()
     {
         OperationResult<IEnumerable<CampusResponse>> result = await campusService.GetAllCampusAsync();
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
 
     [HttpGet("campus/active")]
@@ -34,7 +34,7 @@ public sealed class AcademicManagementController(
     {
         OperationResult<IEnumerable<CampusResponse>> result = await campusService.GetAllActiveCampusAsync();
         return !result.IsFailure
-            ? Ok(result.Value)
+            ? Ok(result)
             : HandleFailure(result);
     }
     
@@ -43,14 +43,14 @@ public sealed class AcademicManagementController(
     public async Task<IActionResult> CreateCampusAsync(CreateCampusRequest request)
     {
         OperationResult<string> result = await campusService.CreateCampusAsync(request);
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
 
     [HttpGet("campus/{id}")]
     public async Task<IActionResult> GetCampusByIdAsync(string id)
     {
         OperationResult<CampusResponse> result = await campusService.GetCampusByIdAsync(id);
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
     
     [Authorize(Roles = nameof(UserRoles.SuperAdmin))]
@@ -76,7 +76,7 @@ public sealed class AcademicManagementController(
     public async Task<IActionResult> GetAllMajorsAsync()
     {
         OperationResult<IEnumerable<MajorResponse>> result = await majorService.GetAllMajorsAsync();
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
 
     [HttpGet("major/active")]
@@ -84,7 +84,7 @@ public sealed class AcademicManagementController(
     {
         OperationResult<IEnumerable<MajorResponse>> result = await majorService.GetAllActiveMajorsAsync();
         return !result.IsFailure
-            ? Ok(result.Value)
+            ? Ok(result)
             : HandleFailure(result);
     }
 
@@ -93,7 +93,7 @@ public sealed class AcademicManagementController(
     {
         OperationResult<IEnumerable<MajorResponse>> result = await majorService.GetMajorsByMajorGroupIdAsync(majorGroupId);
         return !result.IsFailure
-            ? Ok(result.Value)
+            ? Ok(result)
             : HandleFailure(result);
     }
     
@@ -101,7 +101,7 @@ public sealed class AcademicManagementController(
     public async Task<IActionResult> GetMajorByIdAsync(string id)
     {
         OperationResult<MajorResponse> result = await majorService.GetMajorByIdAsync(id);
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
     
     [Authorize(Roles = nameof(UserRoles.SuperAdmin))]
@@ -109,7 +109,7 @@ public sealed class AcademicManagementController(
     public async Task<IActionResult> CreateMajorAsync(CreateMajorRequest request)
     {
         OperationResult<string> result = await majorService.CreateMajorAsync(request);
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
 
     [Authorize(Roles = nameof(UserRoles.SuperAdmin))]
@@ -136,7 +136,7 @@ public sealed class AcademicManagementController(
     public async Task<IActionResult> GetAllMajorGroupsAsync()
     {
         OperationResult<IEnumerable<MajorGroupResponse>> result = await majorGroupService.GetAllMajorGroupsAsync();
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
 
     [HttpGet("majorgroup/active")]
@@ -144,14 +144,14 @@ public sealed class AcademicManagementController(
     {
         OperationResult<IEnumerable<MajorGroupResponse>> result = await majorGroupService.GetAllActiveMajorGroupsAsync();
         return !result.IsFailure
-            ? Ok(result.Value)
+            ? Ok(result)
             : HandleFailure(result);
     }
     [HttpGet("majorgroup/{id}")]
     public async Task<IActionResult> GetMajorGroupByIdAsync(string id)
     {
         OperationResult<MajorGroupResponse> result = await majorGroupService.GetMajorGroupByIdAsync(id);
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
 
     [Authorize(Roles = nameof(UserRoles.SuperAdmin))]
@@ -159,7 +159,7 @@ public sealed class AcademicManagementController(
     public async Task<IActionResult> CreateMajorGroupAsync(CreateMajorGroupRequest request)
     {
         OperationResult<string> result = await majorGroupService.CreateMajorGroupAsync(request);
-        return !result.IsFailure ? Ok(result.Value) : HandleFailure(result);
+        return !result.IsFailure ? Ok(result) : HandleFailure(result);
     }
 
     [Authorize(Roles = nameof(UserRoles.SuperAdmin))]
@@ -202,7 +202,7 @@ public sealed class AcademicManagementController(
     {
         OperationResult<IEnumerable<CapstoneResponse>> result = await capstoneService.GetCapstonesByMajorIdAsync(majorId);
         return !result.IsFailure
-            ? Ok(result.Value)
+            ? Ok(result)
             : HandleFailure(result);
     }
     
@@ -263,7 +263,7 @@ public sealed class AcademicManagementController(
     {
         OperationResult<SemesterResponse> result = await semesterService.GetSemesterByIdAsync(semesterId);
         return !result.IsFailure
-            ? Ok(result.Value)
+            ? Ok(result)
             : HandleFailure(result);
     }
     
@@ -287,7 +287,7 @@ public sealed class AcademicManagementController(
     {
         OperationResult<SemesterResponse> result = await semesterService.UpdateSemesterAsync(request);
         return !result.IsFailure
-            ? Ok(result.Value)
+            ? Ok(result)
             : HandleFailure(result);
     }
 
