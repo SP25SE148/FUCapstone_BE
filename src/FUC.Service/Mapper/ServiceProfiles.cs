b6e69010c33ca5cf6955f637834a75a829b3f276
CGBin0305 <huyndhse173531@fpt.edu.vn>
2025-02-24 18:31:56 +0700

    add stuff endpoint

--- src/FUC.Service/Mapper/ServiceProfiles.cs
+++ src/FUC.Service/Mapper/ServiceProfiles.cs
@@ -31,29 +31,6 @@ public class ServiceProfiles : Profile
         // Semester mapping
         CreateMap<Semester, SemesterResponse>();
         
-        // Group mapping
-        CreateMap<Group, GroupResponse>()
-            .ForMember(dest => dest.SemesterName,
-                opt => opt
-                    .MapFrom(src => src.Semester.Name))
-            
-            .ForMember(dest => dest.MajorName,
-                opt => opt
-                    .MapFrom(src => src.Major.Name))
-            
-            .ForMember(dest => dest.CampusName,
-                opt => opt
-                    .MapFrom(src => src.Campus.Name))
-            
-            .ForMember(dest => dest.CapstoneName,
-                opt => opt
-                    .MapFrom(src => src.Capstone.Name))
-            
-            .ForMember(dest => dest.MemberEmailList,
-                opt => opt
-                    .MapFrom(src =>  src.GroupMembers
-                        .Select(gm => gm.Student.Email)
-                        .ToList()));
         
         // Student mapping
         CreateMap<Student, StudentResponseDTO>()
