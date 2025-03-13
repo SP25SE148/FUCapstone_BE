072cfb524c3eca5e0192b9ee627c975e029da22d
CGBin0305 <huyndhse173531@fpt.edu.vn>
2025-03-13 20:11:55 +0700

    fix get group information

--- src/FUC.Service/Abstractions/IGroupMemberService.cs
+++ src/FUC.Service/Abstractions/IGroupMemberService.cs
@@ -6,8 +6,9 @@ namespace FUC.Service.Abstractions;
 
 public interface IGroupMemberService
 {
-    Task<OperationResult<Guid>> CreateBulkGroupMemberAsync(CreateGroupMemberRequest request);
+    Task<OperationResult<Guid>> CreateGroupMemberByLeaderAsync(CreateGroupMemberByLeaderRequest request);
     Task<OperationResult> UpdateGroupMemberStatusAsync(UpdateGroupMemberRequest request);
     Task<OperationResult<GroupMemberRequestResponse>> GetGroupMemberRequestByMemberId();
+    Task<OperationResult<Guid>> CreateGroupMemberByMemberAsync(CreateGroupMemberByMemberRequest request);
     Task<OperationResult<IEnumerable<GroupMember>>> GetGroupMemberByGroupId(Guid groupId);
 }
