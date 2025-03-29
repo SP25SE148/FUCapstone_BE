102bbe6afc8409c0ea08f0a8b205dc8fb7d2f154
CGBin0305 <huyndhse173531@fpt.edu.vn>
2025-03-29 14:14:34 +0700

    add review calendar job & fix review calendar response

--- src/FUC.Service/Consumers/RequestExpirationEventConsumer.cs
+++ src/FUC.Service/Consumers/RequestExpirationEventConsumer.cs
@@ -3,6 +3,7 @@ using FUC.Common.Contracts;
 using FUC.Common.Options;
 using FUC.Data.Data;
 using FUC.Data.Entities;
+using FUC.Data.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -28,10 +29,10 @@ public class TopicRequestExpirationEventConsumer : BaseEventConsumer<TopicReques
         try
         {
             _logger.LogInformation("Starting change status of TopicRequest {Id}",
-            message.TopicRequestId);
+                message.TopicRequestId);
 
             var topicRequest = await _fucDbContext.Set<TopicRequest>().FirstOrDefaultAsync(
-                    x => x.Id == message.TopicRequestId, default);
+                x => x.Id == message.TopicRequestId, default);
 
             ArgumentNullException.ThrowIfNull(topicRequest);
 
@@ -44,11 +45,12 @@ public class TopicRequestExpirationEventConsumer : BaseEventConsumer<TopicReques
             await _fucDbContext.SaveChangesAsync();
 
             _logger.LogInformation("End change status of TopicRequest {Id}",
-            message.TopicRequestId);
+                message.TopicRequestId);
         }
-        catch (Exception ex) 
+        catch (Exception ex)
         {
-            _logger.LogError("Expiration TopicRequest {Id} fail with error {Message}", message.TopicRequestId, ex.Message);
+            _logger.LogError("Expiration TopicRequest {Id} fail with error {Message}", message.TopicRequestId,
+                ex.Message);
 
             throw;
         }
@@ -74,10 +76,10 @@ public class JoinGroupRequestExpirationEventConsumer : BaseEventConsumer<JoinGro
         try
         {
             _logger.LogInformation("Starting change status of JoinGroupRequest {Id}",
-            message.JoinGroupRequestId);
+                message.JoinGroupRequestId);
 
             var request = await _fucDbContext.Set<JoinGroupRequest>().FirstOrDefaultAsync(
-                    x => x.Id == message.JoinGroupRequestId, default);
+                x => x.Id == message.JoinGroupRequestId, default);
 
             ArgumentNullException.ThrowIfNull(request);
 
@@ -90,11 +92,12 @@ public class JoinGroupRequestExpirationEventConsumer : BaseEventConsumer<JoinGro
             await _fucDbContext.SaveChangesAsync();
 
             _logger.LogInformation("End change status of JoinGroupRequest {Id}",
-            message.JoinGroupRequestId);
+                message.JoinGroupRequestId);
         }
         catch (Exception ex)
         {
-            _logger.LogError("Expiration JoinGroupRequest {Id} fail with error {Message}", message.JoinGroupRequestId, ex.Message);
+            _logger.LogError("Expiration JoinGroupRequest {Id} fail with error {Message}", message.JoinGroupRequestId,
+                ex.Message);
 
             throw;
         }
@@ -120,10 +123,10 @@ public class GroupMemberExpirationEventConsumer : BaseEventConsumer<GroupMemberE
         try
         {
             _logger.LogInformation("Starting change status of GroupMember {Id}",
-            message.GroupMemberId);
+                message.GroupMemberId);
 
             var request = await _fucDbContext.Set<GroupMember>().FirstOrDefaultAsync(
-                    x => x.Id == message.GroupMemberId);
+                x => x.Id == message.GroupMemberId);
 
             ArgumentNullException.ThrowIfNull(request);
 
@@ -138,7 +141,7 @@ public class GroupMemberExpirationEventConsumer : BaseEventConsumer<GroupMemberE
                 await _fucDbContext.SaveChangesAsync();
 
                 _logger.LogInformation("End change status of GroupMember {Id}",
-                message.GroupMemberId);
+                    message.GroupMemberId);
             }
             else
             {
@@ -147,10 +150,56 @@ public class GroupMemberExpirationEventConsumer : BaseEventConsumer<GroupMemberE
         }
         catch (Exception ex)
         {
-            _logger.LogError("Expiration GroupMember {Id} fail with error {Message}", message.GroupMemberId, ex.Message);
+            _logger.LogError("Expiration GroupMember {Id} fail with error {Message}", message.GroupMemberId,
+                ex.Message);
 
             throw;
         }
     }
 }
 
+public class ReviewCalendarExpirationEventConsumer : BaseEventConsumer<ReviewCalendarExpirationEvent>
+{
+    private readonly ILogger<ReviewCalendarExpirationEventConsumer> _logger;
+    private readonly FucDbContext _fucDbContext;
+
+    public ReviewCalendarExpirationEventConsumer(
+        ILogger<ReviewCalendarExpirationEventConsumer> logger,
+        FucDbContext fucDbContext,
+        IOptions<EventConsumerConfiguration> options) : base(logger, options)
+    {
+        _logger = logger;
+        _fucDbContext = fucDbContext;
+        _fucDbContext.DisableInterceptors = true;
+    }
+
+    protected override async Task ProcessMessage(ReviewCalendarExpirationEvent message)
+    {
+        try
+        {
+            var reviewCalendar = _fucDbContext.Set<ReviewCalendar>().FirstOrDefault(
+                x => x.Id == message.ReviewCalendarId);
+
+            ArgumentNullException.ThrowIfNull(reviewCalendar);
+            if (reviewCalendar.Status == ReviewCalendarStatus.InProgress)
+            {
+                reviewCalendar.Status = ReviewCalendarStatus.Done;
+                _fucDbContext.Update(reviewCalendar);
+                await _fucDbContext.SaveChangesAsync();
+                _logger.LogInformation("End change status of GroupMember {Id}",
+                    message.ReviewCalendarId);
+            }
+            else
+            {
+                await Task.CompletedTask;
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Expiration GroupMember {Id} fail with error {Message}", message.ReviewCalendarId,
+                e.Message);
+
+            throw;
+        }
+    }
+}
