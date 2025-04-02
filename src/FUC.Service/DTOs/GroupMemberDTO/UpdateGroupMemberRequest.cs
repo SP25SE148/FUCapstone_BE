09a9da4c88b22c447978c2cf704db4e0120ced6d
CGBin0305 <huyndhse173531@fpt.edu.vn>
2025-04-03 02:02:25 +0700

    fix updateGroupMemberStatus

--- src/FUC.Service/DTOs/GroupMemberDTO/UpdateGroupMemberRequest.cs
+++ src/FUC.Service/DTOs/GroupMemberDTO/UpdateGroupMemberRequest.cs
@@ -4,7 +4,5 @@ namespace FUC.Service.DTOs.GroupMemberDTO;
 
 public sealed record UpdateGroupMemberRequest(
     Guid Id,
-    string? MemberId,
-    string? NewLeaderId,
     Guid GroupId,
     GroupMemberStatus Status);
